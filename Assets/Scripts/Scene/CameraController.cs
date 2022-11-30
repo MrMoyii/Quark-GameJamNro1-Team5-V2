f2789ad0806c290ce19ad0eaa3f162664b6041a4
Omar Moyano <omarmoyano7w@gmail.com>
2022-11-29 23:51:44 -0300

    Camara fix
    Agregado de script para la cámara

---
+++ Assets/Scripts/Scene/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    Vector3 cameraPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraPosition = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraNewPosition  = player.transform.position;
+
+        float jumpYPosition = cameraPosition.y + cameraNewPosition.y  ;
+
+        gameObject.transform.position = new Vector3(cameraPosition.x, jumpYPosition, cameraNewPosition.z);
+    }
+}
