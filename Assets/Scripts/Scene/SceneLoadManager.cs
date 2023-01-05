951190befdad366696619f83984443a97b8fb376
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-05 14:08:28 -0300

    TransitionSceneMainMenu

---
+++ Assets/Scripts/Scene/SceneLoadManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoadManager : MonoBehaviour
+{
+    [SerializeField] private float transitionTime = 1f;
+    private Animator transitionScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transitionScene = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            LoadNextScene();
+        }
+    }
+    public void LoadNextScene()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        StartCoroutine(SceneLoad(nextSceneIndex));
+    }
+
+    public IEnumerator SceneLoad(int sceneIndex)
+    {
+        transitionScene.SetTrigger("StartTransition");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
--- Assets/Scripts/View/MainMenu/MainMenu.cs
+++ Assets/Scripts/View/MainMenu/MainMenu.cs
@@ -5,14 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private float transitionTime = 1f;
+    [SerializeField] private Animator transitionScene;
     public void GameScene()
     {
-        SceneManager.LoadScene("TestScene");
+        StartCoroutine(SceneLoad("TestScene"));
         Time.timeScale = 1f;
     }
 
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public IEnumerator SceneLoad(string sceneIndex)
+    {
+        transitionScene.SetTrigger("StartTransition");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(sceneIndex);
     }
 }
